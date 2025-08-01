de00611eecca20443e9a5e512ffcd4be24782354
amattson2023F <amattson@vt.edu>
2025-08-01 14:49:50 -0400

    git hub pages things

--- Desktop/TuneTrition/tunetrition/src/main.jsx
+++ Desktop/TuneTrition/tunetrition/src/main.jsx
@@ -2,16 +2,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Callback from "./components/Callback.jsx";
 import Dashboard from "./components/Dashboard.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter >
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/callback" element={<Callback />} />
       <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter >
 );
